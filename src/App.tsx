c977f71d811161477bd0d10d48d824467128b504
Allan <allangeek@gmail.com>
2023-09-26 15:18:34 -0300

    Get prompts from backend

--- src/App.tsx
+++ src/App.tsx
@@ -12,95 +12,86 @@ import {
 } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
   return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='px-6 py-3 flex items-center justify-between border-b'>
-        <h1 className='text-xl font-bold'>upload.ai</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-        <div className='flex items-center gap-3'>
-          <span className='text-sm text-muted-foreground'>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
             Desenvolvido com ♥ em São Paulo
           </span>
 
-          <Separator orientation='vertical' className='h-6 ' />
+          <Separator orientation="vertical" className="h-6 " />
 
-          <Button variant='outline'>
-            <Github className='w-4 h-4 mr-2' />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
       </div>
 
-      <main className='flex-1 p-6 flex gap-6'>
-        <div className='flex flex-col flex-1 gap-4'>
-          <div className='grid grid-rows-2 gap-4 flex-1'>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
-              className='resize-none p-4 leading-relaxed'
-              placeholder='Inclua o prompt para a IA...'
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
             />
             <Textarea
-              className='resize-none p-4 leading-relaxed'
-              placeholder='Resultado gerado pela IA...'
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
               readOnly
             />
           </div>
 
-          <p className='text-sm text-muted-foreground'>
+          <p className="text-sm text-muted-foreground">
             Lembre-se: você pode utilizara variável{' '}
-            <code className='text-violet-400'>{'{transcription}'}</code> no seu
+            <code className="text-violet-400">{'{transcription}'}</code> no seu
             prompt para adicionar o conteúdo da transcrição do vídeo
             selecionado.
           </p>
         </div>
 
-        <aside className='w-80 space-y-6'>
+        <aside className="w-80 space-y-6">
           <VideoInputForm />
 
           <Separator />
 
-          <form className='space-y-6'>
-            <div className='space-y-2'>
+          <form className="space-y-6">
+            <div className="space-y-2">
               <Label>Prompt</Label>
 
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder='Selecione um prompt...' />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value='title'>Título do YouTube</SelectItem>
-                  <SelectItem value='description'>
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect />
             </div>
 
-            <div className='space-y-2'>
+            <div className="space-y-2">
               <Label>Modelo</Label>
 
-              <Select disabled defaultValue='gpt3.5'>
+              <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value='gpt3.5'>GPT 3.5-turbo 16k</SelectItem>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
 
-              <span className='block text-xs text-muted-foreground italic'>
+              <span className="block text-xs text-muted-foreground italic">
                 Você poderá customizar essa opção em breve
               </span>
             </div>
 
             <Separator />
 
-            <div className='space-y-4'>
+            <div className="space-y-4">
               <Label>Temperatura</Label>
               <Slider min={0} max={1} step={0.1} />
 
-              <span className='block text-xs text-muted-foreground italic leading-relaxed'>
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e
                 com possíveis erros.
               </span>
@@ -108,8 +99,8 @@ export function App() {
 
             <Separator />
 
-            <Button type='submit' className='w-full'>
-              Executar <Wand2 className='w-4 h-4 ml-2' />
+            <Button type="submit" className="w-full">
+              Executar <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
